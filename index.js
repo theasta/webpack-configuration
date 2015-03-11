ff31a7c2245600eeee944a5a7338e03c04f47c25
Alexandrine Boissière <theasta@gmail.com>
2015-03-10 23:19:04 -0700

    options.verbose outputs list of loaders that need to be installed

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var VersionRetrievalPlugin = require("version-retrieval-webpack-plugin");
-var _ = require('lodash');
+var _ = require("lodash");
+var LOADERS = require("./lib/constants/loaders");
+var loadersUtils = require("./lib/loaders");
 
-/*
- options.extractCSS
- options.longTermCaching
- options.debug
- options.devtool
- options.commonsChunk
- options.versionMap
+/**
+ * @param {object} options
+ * @param {object|string} [options.autoprefixer=false] - Use autoprefixer
+ * @param {boolean} [options.commonsChunk=false] - Create a common chunk
+ * @param {boolean} [options.extractCSS=false] - Extract CSS from JS
+ * @param {string} [options.extractCSSPublicPath=""] - Public Path used when creating external CSS files
+ * @param {boolean} [options.debug=false] - Enable or disable debug mode
+ * @param {string} [options.devtool="eval"] (eval, source-map)
+ * @param {boolean} [options.longTermCaching=false]
+ * @param {string} [options.style="css"] (css, less)
+ * @param {boolean} [options.versionMap] - Path to the version map file
+ * @param {boolean} [options.verbose=false] - Will output the list of loaders to install through npm
  */
 module.exports = function (baseConfig, options) {
 
@@ -22,8 +29,14 @@ module.exports = function (baseConfig, options) {
   var config = _.merge({}, defaultConfig, baseConfig || {});
 
   var defaultOptions = {
-    longTermCaching: false,
+    autoprefixer: false,
+    commonsChunk: false,
     extractCSS: false,
+    extractCSSPublicPath: "",
+    debug: false,
+    devtool: "eval",
+    longTermCaching: false,
+    style: "css",
     react: true
   };
 
@@ -49,23 +62,45 @@ module.exports = function (baseConfig, options) {
     new webpack.optimize.OccurrenceOrderPlugin(true)
   ];
 
+  var styleRegExp;
+  var styleLoader = loadersUtils.add(LOADERS.STYLE);
+  var stylesLoader = [loadersUtils.add(LOADERS.CSS)];
+  if (options.style === "css") {
+    styleRegExp = /\.css$/;
+
+    if (options.autoprefixer) {
+      var autoprefixerLoader = loadersUtils.add(LOADERS.AUTOPREFIXER);
+      if (_.isString(options.autoprefixer)) {
+        autoprefixerLoader += "?browsers=" + options.autoprefixer;
+      } else if (_.isArray(options.autoprefixer)) {
+        autoprefixerLoader += "?" + JSON.stringify({
+          browsers: options.autoprefixer
+        });
+      }
+      stylesLoader.push(autoprefixerLoader);
+    }
+  } else if (options.style === "less") {
+    styleRegExp = /\.less$/;
+    stylesLoader.push(loadersUtils.add(LOADERS.LESS));
+  }
+
   if (options.extractCSS) {
     plugins.push(new ExtractTextPlugin(getNameWithChunk("[name]", "css")));
     extraLoaders.push(
       {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader", {publicPath: ""})
+        test: styleRegExp,
+        loader: ExtractTextPlugin.extract(styleLoader, stylesLoader.join("!"), {publicPath: ""})
       }
     );
   } else {
-    extraLoaders.push({ test: /\.css$/, loader: "style-loader!css-loader" });
+    extraLoaders.push({ test: styleRegExp, loader: stylesLoader.unshift(styleLoader).join("!") });
   }
 
   // do more than config.debug
   if (options.debug) {
     config.output.pathinfo = true;
     config.debug = true;
-    config.devtool = options.devtool || 'eval';
+    config.devtool = options.devtool;
   } else {
     config.output.pathinfo = false;
     config.debug = false;
@@ -77,11 +112,15 @@ module.exports = function (baseConfig, options) {
   }
 
   if (options.react) {
+    var reactLoaders = [loadersUtils.add(LOADERS.JSX) + "?harmony"];
+    if (options.hot) {
+      reactLoaders.unshift(loadersUtils.add(LOADERS.REACT_HOT));
+    }
     config.resolve.extensions.push(".jsx");
     extraLoaders.push(
       {
         test: /\.jsx$/,
-        loader: options.hot ? "react-hot-loader!jsx-loader?harmony" : "jsx-loader?harmony"
+        loader: reactLoaders.join("!")
       }
     );
   }
@@ -100,7 +139,7 @@ module.exports = function (baseConfig, options) {
   if (_.isString(options.stats)) {
     plugins.push(function () {
       this.plugin("done", function (stats) {
-        var fs = require('fs');
+        var fs = require("fs");
         var jsonStats = stats.toJson({
           chunkModules: true,
           exclude: [/node-libs-browser/]
@@ -120,6 +159,10 @@ module.exports = function (baseConfig, options) {
 
   config.plugins = plugins;
   config.module.loaders = config.module.loaders.concat(extraLoaders);
+
+  if (options.verbose) {
+    console.log("Following loaders need to be installed:", loadersUtils.list())
+  }
 
   return config;
 };
